6027c7309b896952c374ccd151852c112e2f09ad
Finnland <simpson.finnegan@gmail.com>
2022-11-26 20:50:46 -0500

    select fonctionne

--- action/StatsAction.php
+++ action/StatsAction.php
@@ -1,18 +1,18 @@
 <?php
-    require_once("action/CommonAction.php");
-    require_once("action/DAO/AnswerDAO.php");
-    class StatsAction extends CommonAction {
+require_once("action/CommonAction.php");
+require_once("action/DAO/AnswerDAO.php");
+class StatsAction extends CommonAction
+{
 
-        public function __construct() {
-            parent::__construct(CommonAction::$VISIBILITY_MEMBER);
-        }
-
-        protected function executeAction() {
-
-            $stats = AnswerDAO::getStats();
-            $_SESSION["stats"]=$stats;
-            
-            return compact("stats");
-            
-        }
+    public function __construct()
+    {
+        parent::__construct(CommonAction::$VISIBILITY_MEMBER);
     }
+
+    protected function executeAction()
+    {
+        $data["stat"] = AnswerDAO::getStats();
+       
+        return compact("data");
+    }
+}
