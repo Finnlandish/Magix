74995fc3fce40d490501813e8f3c3ff742d8a476
Finnlandish <simpson.finnegan@gmail.com>
2023-07-18 16:23:37 -0400

    update imgs

--- js/lobby.js
+++ js/lobby.js
@@ -1,4 +1,4 @@
-import TiledImage from './TiledImage.js'
+import TiledImage from "./TiledImage.js";
 // =======================================================
 // Animation in a canvas
 // -------------------------------------------------------
@@ -16,15 +16,23 @@ let node = document.createElement("div");
 document.getElementById("container").append(node);
 let doc = document.getElementById("container");
 
-let tiledImageDOM = new TiledImage("img/pokemon/chansey.png", 4, 1, 300, true, 1, node);
+let tiledImageDOM = new TiledImage(
+  "img/pokemon/chansey.png",
+  4,
+  1,
+  300,
+  true,
+  1,
+  node
+);
 node.id = "chansey";
 tiledImageDOM.changeRow(0);
 tiledImageDOM.setMinMaxDimensions(0, 50, 159, 159);
 
 // Logic where the sprite changes row after animating through its columns
 tiledImageDOM.changeMinMaxInterval(0, 4, () => {
-    tiledImageDOM.changeRow(0);
-    tiledImageDOM.changeMinMaxInterval(0, 4);
+  tiledImageDOM.changeRow(0);
+  tiledImageDOM.changeMinMaxInterval(0, 4);
 });
 
 let x = 340;
@@ -32,90 +40,80 @@ let y = 280;
 let depx = -0.5;
 let depy = 0.4;
 const tickDOM = () => {
+  x += depx;
+  y += depy;
 
-    x += depx
-    y += depy
+  if (y >= 685) {
+    doc.style.zIndex = "10";
+    tiledImageDOM.setMinMaxDimensions(0, 50, 160, 160);
 
-    if (y >= 685) {
-        doc.style.zIndex = "10"
-        tiledImageDOM.setMinMaxDimensions(0, 50, 160, 160);
-
-        depx = 0.5
-        depy = 0.08
-        if (x >= 340) {
-            depx = 0
-            depy = 0
-        }
+    depx = 0.5;
+    depy = 0.08;
+    if (x >= 340) {
+      depx = 0;
+      depy = 0;
     }
+  }
 
+  tiledImageDOM.tick(x, y);
 
-    tiledImageDOM.tick(x, y);
-
-    window.requestAnimationFrame(tickDOM);
-}
+  window.requestAnimationFrame(tickDOM);
+};
 
 tickDOM();
-
 
 //audio
 let chansey = document.getElementById("chansey");
 var a = document.getElementById("chanseyAudio");
 
-
 chansey.onclick = () => {
-    playChansey()
-}
-
+  playChansey();
+};
 
 function playChansey() {
-    a.play();
+  a.play();
 }
-
 
 document.onload = () => {
-    getusername()
-}
+  getusername();
+};
 
 function getusername() {
-    document.getElementById("name").innerHTML = JSON.stringify(window.localStorage.getItem("username"));
+  document.getElementById("name").innerHTML = JSON.stringify(
+    window.localStorage.getItem("username")
+  );
 }
 
+var stat = document.getElementById("statmp3");
+var pvp = document.getElementById("pvpmp3");
+var pve = document.getElementById("pvemp3");
 
-
-var stat = document.getElementById("statmp3")
-var pvp = document.getElementById("pvpmp3")
-var pve = document.getElementById("pvemp3")
-
-
-let statb = document.getElementById("stats")
-let pvpb = document.getElementById("jouer")
-let pveb = document.getElementById("practice")
-
-
+let statb = document.getElementById("stats");
+let pvpb = document.getElementById("jouer");
+let pveb = document.getElementById("practice");
 
 statb.onmouseenter = () => {
-    stat.play();
-}
+  stat.play();
+};
 statb.onmouseleave = () => {
-    stat.pause();
-}
+  stat.pause();
+};
 pvpb.onmouseenter = () => {
-    pvp.play();
-}
+  pvp.play();
+};
 pvpb.onmouseleave = () => {
-    console.log("pvp")
+  console.log("pvp");
 
-    pvp.pause();
-}
-
+  pvp.pause();
+};
 
 pveb.onmouseenter = () => {
-    pve.play();
-}
+  pve.play();
+};
 pveb.onmouseleave = () => {
-    console.log("pve")
-    pve.pause();
-}
+  console.log("pve");
+  pve.pause();
+};
 stat.volume = 0.05;
 pvp.volume = 0.05;
 pve.volume = 0.05;
